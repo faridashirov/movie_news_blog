e5bc05c00d66f3e77dbfd72f45c6b373660a92de
Farid Ashirov <farid.ashirov31@gmail.com>
2025-07-21 12:58:59 +0400

    Refactor: create accounts app for user management

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('new_post/', views.new_post, name="new_post"),
     path('update/<int:pk>/', views.UpdatePost.as_view(), name="update_post"), 
     path('delete/<int:pk>/', views.delete_post, name="delete_post"), 
-    path('login/', views.loginview, name="login"),
-    path('signup/', views.signupview, name="signup"),
-    path('logout/', views.logoutview, name="logout")
 ]
